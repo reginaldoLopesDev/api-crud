dd19d42b078da02c48200c51019f4006d5ddc321
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-03-29 18:00:16 -0300

    Criando arquivo de rotas e exportando

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
 
+const routes = require("./routes/routes");
+
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
 database.on("error", (error) => {
-  console.log(`An error ocurred: ${error.message}\n`);
+  console.log(`An error has ocurred: ${error.message}\n`);
   console.log(error);
 });
 
@@ -19,6 +21,7 @@ database.once("connected", () => {
 const app = express();
 
 app.use(express.json());
+app.use("/api", routes);
 
 app.listen(3000, () => {
   console.log(`Server iniciado, escutando porta ${3000}`);
