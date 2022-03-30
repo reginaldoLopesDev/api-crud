8f75218b234e792b7dd91e4664bbbbb004d6d05b
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-03-30 11:33:04 -0300

    Criando arquivo de rotas e métodos iniciais

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,29 @@ const express = require("express");
 
 const router = express.Router();
 
+router.post("/save", (req, res) => {
+  console.log(`Método post`);
+  res.send("Salvar dados.");
+});
+
+router.get("/all", (req, res) => {
+  console.log(`Método get, retornar todos os dados.`);
+  res.send("Retornar todos os dados.");
+});
+
+router.get("/byId", (req, res) => {
+  console.log(`Método get, retornar apenas os dados requisitados pelo Id`);
+  res.send("Retornar dados requisitados.");
+});
+
+router.patch("/updateById/:id", (req, res) => {
+  console.log(`Método patch, atualizar os dados pelo Id`);
+  res.send("Atualizar dados pelo Id.");
+});
+
+router.delete("/deleteById/:id", (req, res) => {
+  console.log(`Método delete, deletar os dados pelo Id.`);
+  res.send("Deletar dados pelo Id.");
+});
+
 module.exports = router;
